691ed5f4f6468b909c06149684d63a2a1f75a410
kasperfryd <kfry@techcollege.dk>
2024-04-22 08:57:39 +0200

    added google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Frontpage } from "./pages/Frontpage";
 import { Layout } from "./layout/Layout";
 import { Aboutpage } from "./pages/Aboutpage";
+import ReactGA from "react-ga4";
 
 function App() {
+  ReactGA.initialize("G-YTQRSQPW5B");
   return (
     <Router>
       <Routes>
