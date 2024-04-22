e8305c3eac6ec7edbd851c341e2a576172d80e75
kasperfryd <kfry@techcollege.dk>
2024-04-22 12:30:05 +0200

    updated router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { Layout } from "./layout/Layout.jsx";
+import { Frontpage } from "./pages/Frontpage.jsx";
+import { Aboutpage } from "./pages/Aboutpage.jsx";
+import "./app.css";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Frontpage />} />
+      <Route path="/home" element={<Frontpage />} />
+      <Route path="/about" element={<Aboutpage />} />
+    </Route>
+  )
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
