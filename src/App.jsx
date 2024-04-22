760e9fbaddd9ad98013a5e6e7e3b0e47426300dc
kasperfryd <kfry@techcollege.dk>
2024-04-22 12:23:31 +0200

    updated router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import { Frontpage } from "./pages/Frontpage";
 import { Layout } from "./layout/Layout";
 import { Aboutpage } from "./pages/Aboutpage";
+
 import "./app.css";
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Frontpage />} />
-          <Route path="/home" element={<Frontpage />} />
-          <Route path="/about" element={<Aboutpage />} />
-        </Route>
-      </Routes>
-    </Router>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Frontpage />} />
+        <Route path="/home" element={<Frontpage />} />
+        <Route path="/about" element={<Aboutpage />} />
+      </Route>
+    )
   );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
