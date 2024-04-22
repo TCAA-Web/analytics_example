ce9ca26722beea61687f827790d00d4f9f206abb
kasperfryd <kfry@techcollege.dk>
2024-04-22 13:20:27 +0200

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
       <ul>
         <li>
           <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
+          <Link to="about">About</Link>
         </li>
       </ul>
     </nav>
